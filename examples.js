a007c9946c745d31a092c3f54de9e075665e8884
Peter Huang <huangcx22@mails.tsinghua.edu.cn>
2025-03-11 18:36:26 +0800

    add fast pretty printer

--- examples.js
+++ examples.js
@@ -8,51 +8,47 @@ import {
   nest,
   align,
   prettyRec,
+  pretty,
 } from "./ppc.js";
 
-const doc1 = flow(tab(1))(t("This"), t("is"), t("pretty."));
-console.group("doc1");
-console.log(doc1);
-for (const pageWidth of [15, 7, 6]) {
-  console.log(`Page width: ${pageWidth}`);
-  const doc = prettyRec(pageWidth, pageWidth, doc1);
-  console.log(doc);
-  console.log({ raw: doc });
-}
-console.groupEnd();
+const docs = [
+  {
+    name: "doc1",
+    doc: flow(tab(1))(t("This"), t("is"), t("pretty.")),
+    widths: [15, 7, 6]
+  },
+  {
+    name: "doc2",
+    doc: group(
+      t("begin"),
+      nest(2)(tab(1), t("work"), tab(1), t("play")),
+      tab(1), t("end")),
+    widths: [19, 18]
+  },
+  {
+    name: "doc3",
+    doc: append(
+      t("Please"),
+      blank(1),
+      align(group(t("align"), tab(1), t("here.")))
+    ),
+    widths: [18, 17]
+  }
+]
 
-const doc2 = group(
-  t("begin"),
-  nest(2)(tab(1), t("work"), tab(1), t("play")),
-  tab(1),
-  t("end")
-);
-console.group("doc2");
-console.log(doc2);
-for (const pageWidth of [21, 20]) {
-  console.log(`Page width: ${pageWidth}`);
-  const doc = prettyRec(pageWidth, pageWidth, doc2);
-  console.log(doc);
-  console.log({ raw: doc });
+for(const test of docs) {
+  console.group(test.name);
+  console.log(test.doc);
+  for(const pageWidth of test.widths) {
+    console.log(`Page width: ${pageWidth}`);
+    console.log("recursive pretty:");
+    const docR = prettyRec(pageWidth, pageWidth, test.doc);
+    console.log(docR);
+    console.log("fast pretty:");
+    const doc = pretty(pageWidth, pageWidth, test.doc);
+    console.log(doc);
+    console.assert(docR === doc);
+    console.log({ cmp: docR, got: doc });
+  }
+  console.groupEnd();
 }
-console.groupEnd();
-
-const doc3 = append(
-  t("Please"),
-  blank(1),
-  align(group(t("align"), tab(1), t("here.")))
-);
-console.group("doc3");
-console.log(doc3);
-for (const pageWidth of [18, 17]) {
-  console.log(`Page width: ${pageWidth}`);
-  const doc = prettyRec(pageWidth, pageWidth, doc3);
-  console.log(doc);
-  console.log({ raw: doc });
-}
-console.groupEnd();
-
-// for (const pageWidth of [15, 10, 7]) {
-//   console.log(`Page width: ${pageWidth}`);
-//   console.log(pretty(pageWidth, doc1));
-// }
--- ppc.js
+++ ppc.js
@@ -5,6 +5,12 @@ class Empty {
   format(_flat, _indent, _state) {
     return "";
   }
+  fstep(_instr, _state, _kont) {
+    return "";
+  }
+  toString() {
+    return "Empty";
+  }
 }
 class Text {
   constructor(text) {
@@ -14,6 +20,13 @@ class Text {
   format(_flat, _indent, state) {
     state.column += this.width;
     return this.text;
+  }
+  fstep(_instr, state, _kont) {
+    state.column += this.width;
+    return this.text;
+  }
+  toString() {
+    return JSON.stringify(this.text);
   }
 }
 class IfFlat {
@@ -27,6 +40,13 @@ class IfFlat {
       ? this.doc1.format(true, indent, state)
       : this.doc2.format(false, indent, state);
   }
+  fstep(instr, _state, _kont) {
+    instr.doc = instr.flat ? this.doc1 : this.doc2;
+    return null;
+  }
+  toString() {
+    return `(IfFlat ${this.doc1} ${this.doc2})`;
+  }
 }
 class HardLine {
   constructor() {
@@ -39,14 +59,30 @@ class HardLine {
     state.lastIndent = indent;
     return "\n" + " ".repeat(indent);
   }
+  fstep(instr, state, _kont) {
+    state.line = state.line + 1;
+    state.column = instr.indent;
+    state.lastIndent = instr.indent;
+    return "\n" + " ".repeat(instr.indent);
+  }
+  toString() {
+    return "HardLine";
+  }
 }
 class Blank {
   constructor(i) {
     this.width = i;
   }
-  format(flat, indent, state) {
+  format(_flat, _indent, state) {
     state.column += this.width;
     return " ".repeat(this.width);
+  }
+  fstep(_instr, state, _kont) {
+    state.column += this.width;
+    return " ".repeat(this.width);
+  }
+  toString() {
+    return `(Blank ${this.width})`;
   }
 }
 class Append {
@@ -59,6 +95,19 @@ class Append {
     const s1 = this.doc1.format(flat, indent, state);
     const s2 = this.doc2.format(flat, indent, state);
     return s1 + s2;
+  }
+  fstep(instr, state, kont) {
+    instr.doc = this.doc1;
+    kont.push({
+      type: KontType.Append1,
+      doc2: this.doc2,
+      flat: instr.flat,
+      indent: instr.indent,
+    });
+    return null;
+  }
+  toString() {
+    return `(Append ${this.doc1} ${this.doc2})`;
   }
 }
 class Group {
@@ -73,15 +122,31 @@ class Group {
       state
     );
   }
+  fstep(instr, state, _kont) {
+    instr.doc = this.doc;
+    instr.flat = instr.flat || state.canFlatten(state.column + this.doc.width);
+    return null;
+  }
+  toString() {
+    return `(Group ${this.doc})`;
+  }
 }
 class Nest {
   constructor(indent, doc) {
     this.indent = indent;
     this.doc = doc;
-    this.width = doc.width + indent;
+    this.width = doc.width;
   }
   format(flat, indent, state) {
     return this.doc.format(flat, Math.max(indent + this.indent, 0), state);
+  }
+  fstep(instr, _state, _kont) {
+    instr.doc = this.doc;
+    instr.indent = Math.max(instr.indent + this.indent, 0);
+    return null;
+  }
+  toString() {
+    return `(Nest ${this.indent} ${this.doc})`;
   }
 }
 class Align {
@@ -91,6 +156,14 @@ class Align {
   }
   format(flat, indent, state) {
     return this.doc.format(flat, state.column, state);
+  }
+  fstep(instr, state, _kont) {
+    instr.doc = this.doc;
+    instr.indent = state.column;
+    return null;
+  }
+  toString() {
+    return `(Align ${this.doc})`;
   }
 }
 
@@ -115,46 +188,40 @@ export const prettyRec = (lineWidth, ribbonWidth, doc) => {
   return doc.format(flat, 0, state);
 };
 
-// defunctionalized version
-const pretty = (pageWidth, doc) => {
-  let restWidth = pageWidth;
-  let flat = false;
-  const kont = [];
-  function apply(k, s, r) {
-    if (k.type === 0) {
-      doc = k.d2;
-      restWidth = r;
-      kont.push({ type: 1, s1: s });
-      return null;
-    } else {
-      return [k.s1 + s, r];
-    }
-  }
+const KontType = {
+  Append1: 0,
+  Append2: 1,
+};
 
+// defunctionalized version
+function applyKontTop(kont, result, instr) {
+  const next = kont.pop();
+  if (next.type === KontType.Append1) {
+    instr.doc = next.doc2;
+    instr.flat = next.flat;
+    instr.indent = next.indent;
+    kont.push({ type: KontType.Append2, s1: result });
+    return null;
+  } else if (next.type === KontType.Append2) {
+    return next.s1 + result;
+  }
+}
+export const pretty = (lineWidth, ribbonWidth, doc) => {
+  let state = new State(lineWidth, ribbonWidth, 0, 0, 0);
+  let instr = { doc, indent: 0, flat: state.canFlatten(doc.width) };
+  const kont = [];
   let ret = null;
-  while (1) {
+  for (let i = 0; i < 500000; i++) {
     if (ret === null) {
-      ret = doc.match({
-        Text: (z) => [z, restWidth - z.length],
-        Line: () => (flat ? [" ", restWidth - 1] : ["\n", pageWidth]),
-        Append: (d1, d2) => {
-          doc = d1;
-          kont.push({ type: 0, d2 });
-          return null;
-        },
-        Group: (d) => {
-          flat = flat || d.width <= restWidth;
-          doc = d;
-          return null;
-        },
-      });
+      ret = instr.doc.fstep(instr, state, kont);
     } else {
-      const [s, r] = ret;
-      if (kont.length === 0) return s;
-      const next = kont.pop();
-      ret = apply(next, s, r);
+      if (kont.length === 0) return ret;
+      ret = applyKontTop(kont, ret, instr);
     }
   }
+  throw new Error(
+    "Exceeded maximum number of steps. Do you really need to pretty print that?"
+  );
 };
 
 // normalizing combinators
